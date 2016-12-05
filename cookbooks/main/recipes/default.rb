89c9cc795c4b78e4d2efd8b679b86d04063ee1e1
Jim Smith <jim@jimsmithdesign.com>
2016-12-05 14:19:01 -0500

    Adding hstore extension to PG

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -4,4 +4,9 @@ include_recipe "redis-yml"
 include_recipe "redis"
 include_recipe "le"
 
+#enable Extension modules for a given Postgresql database
+if ['solo','db_master', 'db_slave'].include?(node[:instance_role])
+  postgresql9_hstore 'bakerbookstore'
+end
+
 include_recipe "bakerbookstore_cron_jobs"
