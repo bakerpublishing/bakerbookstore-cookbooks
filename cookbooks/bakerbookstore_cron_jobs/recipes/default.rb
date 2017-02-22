4081c9f5ff303b972c22e3da78d4c2276a21c2c4
Jim Smith <jim@jimsmithdesign.com>
2017-02-22 10:47:45 -0500

    Adding cronjob to export orders for bookstore manager

--- cookbooks/bakerbookstore_cron_jobs/recipes/default.rb
+++ cookbooks/bakerbookstore_cron_jobs/recipes/default.rb
@@ -7,9 +7,15 @@ Chef::Log.info "[bakerbookstore_cron_jobs] Adding cron jobs for BakerBookStore"
 if node[:name] == 'Utility'
   cron 'import' do
     weekday '4' # Thursday
-    hour '20' # 8 pm
-    minute '0'
-    user 'deploy'
+    hour    '20' # 8 pm
+    minute  '0'
+    user    'deploy'
     command "cd /data/bakerbookstore/current && RAILS_ENV=#{node[:environment][:framework_env]} bundle exec rake import:start"
+  end
+
+  cron 'export_bookstore_manager_orders' do
+    minute  '0' # Run on the hour, every hour
+    user    'deploy'
+    command "cd /data/bakerbookstore/current && RAILS_ENV=#{node[:environment][:framework_env]} bundle exec rake export:bookstore_manager:orders"
   end
 end
